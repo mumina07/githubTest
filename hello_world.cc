7b622037a02e9529f5b19cee9f97a8afb37c6003
mumina07 <mmahmood.ln1@gmail.com>
2021-06-23 16:57:54 +0600

    Add files via upload

---
+++ hello_world.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "HELLO WORLD!" << endl << "Enter a number: ";
+    cin >> n;
+
+    cout << "The number entered is: " << n << endl;
+
+    return 0;
+}
